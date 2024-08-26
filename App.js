13d589f9310617d9d6510e2da3c3c5b53f0a04db
Satu Kulta <satu.kulta@myy.haaga-helia.fi>
2024-08-26 21:41:23 +0300

    Ostoslista + fonttimuotoilukokeiluja

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  console.log(item, items)
+
+  const handleAdd = () => {
+    if (item) {
+      setItems([...items, item]);
+      setItem('');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Enter item'
+          onChangeText={text => setItem(text)}
+          value={item}
+        />
+        <View style={styles.buttonContainer}>
+          <Button title='ADD' onPress={handleAdd} />
+          <Button title='CLEAR' onPress={() => setItems([])}/>
+        </View>
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList 
+          data={items}
+          renderItem={({item}) => <Text style={styles.list}>- {item}</Text>}
+          ListHeaderComponent={<Text style={styles.listHeader}>Shopping List</Text>}
+          ListEmptyComponent={<Text style={styles.listEmpty}>No items to get...</Text>}
+        />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +48,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+  },
+  inputContainer: {
+    flex: 1,
+  },
+  input: {
+    fontSize: 18,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 8
+  },
+  buttonContainer: {
+    marginTop: 40,
+    flexDirection: 'row',
+    alignSelf: 'center',
+    gap: 10
+  },
+  listContainer: {
+    flex: 3,
+  },
+  listHeader: {
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#008b8b',
+    textShadowColor: '#404040',
+    textShadowOffset: {width: 1, height: -1},
+    textShadowRadius: 1,
+  },
+  listEmpty: {
+    fontSize: 15,
+    marginTop: 20,
+  },
+  list: {
+    fontSize: 18,
+    textAlign: 'left',
+    marginLeft: 20,
   },
 });
